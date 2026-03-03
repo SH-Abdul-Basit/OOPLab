27d57bd64f30dfe01d435461f378fe14bb4e8837
Y2kSurviver <abasitmarkting181@gmail.com>
2026-03-03 15:29:16 +0500

    OOP lab 04 exercise

---
+++ src/Lab04/Practice.java
@@ -0,0 +1,95 @@
+package Lab04;
+
+import java.util.Scanner;
+
+public class Practice {
+	public static void main(String[] args) {
+		// take input from users
+//		Scanner input = new Scanner(System.in);
+//		System.out.println("Enter your marks: ");
+//		double marks = input.nextDouble();
+//		// ternary operator checks if marks is greater than 40
+//		String result = (marks > 40) ? "pass" : "fail";
+//		System.out.println("You " + result + " the exam.");
+//		input.close();
+		
+		// Exercise 1
+//		int a, b; 
+//		a = 10;
+//		b = (a == 1) ? 20 : 30;
+//		System.out.println(b);
+//		b = (a == 10) ? 20 : 30;
+//		System.out.println(b);
+		
+		// Exercise 2
+//		int x = 18; // x= 18 
+//		int y= x++; // y = 18 x= 19
+//		int z=++y; // y = 19 x = 19
+//		System.out.println("x: " +x);
+//		System.out.println("y: " +y);
+//		System.out.println("z: " +z);
+		
+		// Exercise 3
+//		int a=10;
+//		int b=-37;
+//		boolean c=true; // Logical operator
+//		System.out.println(~a);
+//		System.out.println(~b);
+//		System.out.println(!c);
+//		
+		// Exercise 4
+//		int i = 10;
+//		int n = ++i%5;
+//		/*
+//		 1. What are the values of i and n when the code is executed?
+//2. What are the final values of i and n if instead of using the postfix increment
+//operator (i++), you use the prefix version (++i)?
+//		 */
+//		
+//		System.out.println(i + " " + n);
+		
+		// Exercise 5
+//		short x1 = 4;
+////		x1=x1+6.6; 
+//		 x1 += 6.6; // No casting needed
+//		System.out.println("x1 is " + x1 + "!");
+//		
+//		// Another example
+//		byte b = 10;
+////		b = b + 10; 
+//		b += 10;
+//		System.out.println(b); 
+
+		// Exercise 6
+		// 1. Evaluate the following Boolean expressions and prints their results.
+//		Boolean b1, b2, b3;
+//		b1 = true || false;
+//		b2 = b1 && (5 > 3);
+//		b3 = b2 && (13 == 6);
+//		System.out.println(b1);
+//		System.out.println(b2);
+//		System.out.println(b3);
+//		// 2. What is the difference between following two statements?
+//		Boolean b4= (100>5) || (6*8>5); // Short circuit
+//		Boolean b5= (100>5) | (6*8>5); // Full 
+//		
+//		System.out.println(b4);
+//		System.out.println(b5);
+		// Exercise 7
+//		System.out.println(10<<2);
+//		System.out.println(10<<3);
+//		System.out.println(20<<2);
+//		System.out.println(15<<4);
+		
+		// Exercise 8
+//		int a=20,b=10,c=0,d=20, e=40, f=30;
+//		System.out.println("a+b/d=" +(a+b/d));
+//		System.out.println("a+b*d-e/f=" +(a+b*d-e/f));
+		
+//		int a=10, b=5, c=1, result;
+//		result = a- ++c- ++b;
+//		System.out.println("result is:" +result);
+		
+		
+	}
+}
--- src/Lab04/Task01.java
+++ src/Lab04/Task01.java
@@ -4,7 +4,7 @@ public class Task01 {
 	public static void main(String[] args) {
 		int a = 5;
 		int b = 4;
-		System.out.println("(a<<2)+(b>>2) = " + (a<<2)+(b>>2));
+		System.out.println((a<<2) + (b>>2));
 		// >> and << does not change the original values
 		System.out.println("(a++ != b++) && (a++ ==b++) = " + ((a++ != b++) && (a++ ==b++)));
 		// Reseting the values
